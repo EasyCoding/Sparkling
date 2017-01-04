0ea54caae378ab75b901c17c24f16c4e852e7dc9
Greg <xztv@mail.ru>
2017-01-04 16:49:53 +0300

    Исправлено отображение информации о категориях и комментариях на главной странице

--- content.php
+++ content.php
@@ -19,7 +19,6 @@
 
 				<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-				<?php if ( 'post' == get_post_type() ) : ?>
 				<div class="entry-meta">
 					<?php sparkling_posted_on(); ?>
                     <?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
@@ -53,7 +52,6 @@
 				<?php endif; ?>
 
 				</div><!-- .entry-meta -->
-				<?php endif; ?>
 			</header><!-- .entry-header -->
 
 			<?php if ( is_search() ) : // Only display Excerpts for Search ?>
